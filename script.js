aa25458ecfb929cd3acaa2eef0e74b102482faf7
SpectreKilo <johnho8788@gmail.com>
2022-12-21 00:21:18 -0500

    added character variables

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // Assignment Code
+// Character Variables
+var lower = "abcdefghijklmnopqrstuvwxyz"
+var UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var symbols = "!@#$%^&*()-_=+]}[{':;/?.>,<"
+var numbers = "1234567890"
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
